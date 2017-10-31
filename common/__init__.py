946d2bc7b305b4325a83edfc21d90bab66e5b139
Roman Ring <inoryy@gmail.com>
2017-10-31 12:07:47 +0200

    add preprocessing code for obs screen / minimap

---
+++ common/__init__.py
@@ -0,0 +1,51 @@
+import numpy as np
+from pysc2.lib import features
+
+
+def n_channels(feats):
+    return n_channels_type('SCREEN', feats['screen']), n_channels_type('MINIMAP', feats['minimap'])
+
+
+def n_channels_type(type, feat_names):
+    cat_channels_in = cat_channels_out = num_channels = 0
+    feats = getattr(features, type + '_FEATURES')
+    for f_name in feat_names:
+        f = getattr(feats, f_name)
+        if f.type == features.FeatureType.CATEGORICAL:
+            cat_channels_out += 1
+            cat_channels_in += f.scale
+        else:
+            num_channels += 1
+    return cat_channels_in, cat_channels_out, num_channels
+
+
+def preprocess_inputs(x, feats):
+    return preprocess_type(x, 'screen', feats), preprocess_type(x, 'minimap', feats)
+
+
+def preprocess_type(x, type, feats):
+    cat_x, num_x = zip(*[preprocess_obs(obs, type, feats[type]) for obs in x])
+    return np.array(cat_x), np.array(num_x)
+
+
+def preprocess_obs(obs, type, feat_names):
+    cat, num = [], []
+    feats = getattr(features, type.upper() + '_FEATURES')
+    for f_name in feat_names:
+        f = getattr(feats, f_name)
+        if f.type == features.FeatureType.CATEGORICAL:
+            cat.append(one_hot(obs.observation[type][f.index], f.scale))
+        else:
+            num.append(obs.observation[type][f.index])
+
+    cat = np.concatenate(cat, axis=2) if len(cat) > 0 else []
+    num = np.array(num).transpose((1, 2, 0)) if len(num) > 0 else []
+
+    return cat, num
+
+
+def one_hot(x, n_classes):
+    out = np.zeros( (x.size, n_classes), dtype=np.uint8)
+    out[np.arange(x.size), x.ravel()] = 1
+    out.shape = x.shape + (n_classes,)
+    return out
