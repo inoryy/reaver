5bc0e0ae06d85a264bc2986f723dcc125e7b4f79
Roman Ring <inoryy@gmail.com>
2017-10-31 12:08:56 +0200

    add (failing) simple RL model implementation

--- agent/rl.py
+++ agent/rl.py
@@ -1,10 +1,13 @@
 from pysc2.agents import base_agent
+from common import preprocess_inputs
 
 
 class RLAgent(base_agent.BaseAgent):
-    def __init__(self, model, algorithm):
+    def __init__(self, model, algorithm, feats):
+        self.feats = feats
         self.model = model
         self.algo = algorithm
 
     def step(self, obs):
-        pass
+        x = preprocess_inputs(obs, self.feats)
+        spatial_action, value = self.model.forward(x)
