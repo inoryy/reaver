bdf867d46beea18b89e17fdcb61ab3d569c89bdc
Roman Ring <inoryy@gmail.com>
2017-11-07 13:56:22 +0200

    add basic support for rl agent configs in play

--- play.py
+++ play.py
@@ -4,7 +4,7 @@ from common.runner import Runner
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("map_name", "MoveToBeacon", "Which map to use.")
-flags.DEFINE_string("agent", "agent.scripted.ParallelMoveToBeacon", "Which agent to run.")
+flags.DEFINE_string("agent", "agent.rl.RLAgent", "Which agent to run.")
 flags.DEFINE_integer("n_envs", 10, "Number of SC2 environments to run in parallel.")
 flags.DEFINE_bool("render", False, "Whether to render with pygame.")
 flags.DEFINE_integer("max_steps", 2000, "Max steps per env.")
@@ -15,6 +15,14 @@ if __name__ == '__main__':
     agent_module, agent_name = FLAGS.agent.rsplit(".", 1)
     agent_cls = getattr(importlib.import_module(agent_module), agent_name)
 
-    agent = agent_cls()
+    agent_args = []
+    if agent_name == 'RLAgent':
+        import tensorflow as tf
+        tf.reset_default_graph()
+        sess = tf.Session()
+        feats = {'screen': ['player_relative', 'unit_hit_points'], 'minimap': ['player_relative', 'height_map']}
+        agent_args = [sess, feats]
+
+    agent = agent_cls(*agent_args)
     runner = Runner(FLAGS.n_envs, FLAGS.map_name, visualize=FLAGS.render)
     runner.run(agent, FLAGS.max_steps)
