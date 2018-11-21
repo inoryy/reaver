ea3ed5c69382b0439aaa68b04dfd58a5921fedf5
Roman Ring <inoryy@gmail.com>
2018-11-21 13:39:08 +0200

    tweak version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='reaver',
-    version='2.0',
+    version='2.0.0',
     author='Roman Ring',
     author_email='inoryy@gmail.com',
     license='MIT',
