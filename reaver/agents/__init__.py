ca6045376f6cd40eb90983d0d0baa873db04277d
Roman Ring <inoryy@gmail.com>
2018-11-21 18:27:36 +0200

    add aliases (abberviations) for agent names

--- reaver/agents/__init__.py
+++ reaver/agents/__init__.py
@@ -2,3 +2,6 @@ from .base import *
 from .random import RandomAgent
 from .a2c import AdvantageActorCriticAgent
 from .ppo import ProximalPolicyOptimizationAgent
+
+A2C = AdvantageActorCriticAgent
+PPO = ProximalPolicyOptimizationAgent
