d05ee9e0a879218843c24df365bb190034a52fd4
Roman Ring <inoryy@gmail.com>
2019-01-01 20:08:01 +0200

    update tests for new API, add separate gin config for cartpole

--- tests/test_convergence.py
+++ tests/test_convergence.py
@@ -14,42 +14,41 @@ class TestConvergence(unittest.TestCase):
         # see https://github.com/tensorflow/tensorflow/issues/16152
         warnings.filterwarnings("ignore", category=DeprecationWarning, module="tensorflow")
 
-        self.n_envs = 4
-        self.batch_sz = 64
-        self.n_updates = 100
+        self.seed = 1234
         self.env = rvr.envs.GymEnv('CartPole-v0')
 
     def test_a2c(self):
         self._test_agent(rvr.agents.A2C)
 
     def test_ppo(self):
-        self._test_agent(rvr.agents.PPO)
+        self._test_agent(rvr.agents.PPO, n_updates=4, minibatch_sz=256)
 
-    def _test_agent(self, agent_cls):
+    def _test_agent(self, agent_cls, **kwargs):
+        _kwargs = dict(learning_rate=0.0007, entropy_coef=0.1, batch_sz=32, gae_lambda=0.0, clip_grads_norm=1.0,
+                       clip_rewards=1.0, normalize_advantages=False, **kwargs)
+
         tf.reset_default_graph()
+        tf.set_random_seed(self.seed)
 
         sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, device_count={'GPU': 0}))
         sess_mgr = rvr.utils.tensorflow.SessionManager(sess, base_path='/tmp/results/', checkpoint_freq=None)
 
-        logger = rvr.utils.StreamLogger(n_envs=self.n_envs, sess_mgr=sess_mgr)
-        logger.streams = []
+        agent = agent_cls(self.env.obs_spec(), self.env.act_spec(), self._model_builder,
+                          rvr.models.MultiPolicy, sess_mgr, n_envs=4, **_kwargs)
+        agent.logger = rvr.utils.StreamLogger(n_envs=4, sess_mgr=sess_mgr)
+        agent.logger.streams = []
 
-        agent = agent_cls(
-            self.env.obs_spec(), self.env.act_spec(), self._model_builder, rvr.models.MultiPolicy, sess_mgr,
-            n_envs=self.n_envs, batch_sz=self.batch_sz, gae_lambda=0.0, clip_grads_norm=100.0, entropy_coef=0.0,
-            value_coef=0.5, optimizer=tf.train.AdamOptimizer(learning_rate=0.005), logger=logger,
-        )
-        self.env._env.seed(0)
-        agent.run(self.env, self.n_updates * agent.traj_len * agent.batch_sz // agent.n_envs)
+        self.env._env.seed(self.seed)
+        agent.run(self.env, 100 * agent.traj_len * agent.batch_sz // agent.n_envs)
 
-        ep_rews = np.array(logger.ep_rews_sum or [0])
+        ep_rews = np.array(agent.logger.ep_rews_sum or [0])
 
         self.assertGreaterEqual(ep_rews.max(), 200.0)
-        self.assertGreaterEqual(ep_rews.mean() + ep_rews.std(), 180.0)
+        self.assertGreaterEqual(ep_rews.mean() + ep_rews.std(), 160.0)
 
     @staticmethod
     def _model_builder(obs_spec, act_spec):
-        return rvr.models.build_mlp(obs_spec, act_spec, activation='tanh', value_separate=True)
+        return rvr.models.build_mlp(obs_spec, act_spec, activation='relu', value_separate=True, normalize_obs=True)
 
 
 if __name__ == '__main__':
