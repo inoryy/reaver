900017d7940424bb688746f1489600bcbb6a8de3
Roman Ring <inoryy@gmail.com>
2019-01-02 12:30:24 +0200

    fix a bug that sometimes caused shared memory objects persist through timesteps

--- reaver/agents/base/running.py
+++ reaver/agents/base/running.py
@@ -22,10 +22,11 @@ class RunningAgent(Agent):
     def _run(self, env, n_steps):
         self.on_start()
         obs, *_ = env.reset()
+        obs = [o.copy() for o in obs]
         for step in range(self.start_step, self.start_step + n_steps):
             action, value = self.get_action_and_value(obs)
             self.next_obs, reward, done = env.step(action)
-            self.on_step(step, obs, action, reward, done, value)
+            self.on_step(step, [o.copy() for o in obs], action, reward, done, value)
             obs = [o.copy() for o in self.next_obs]
         env.stop()
         self.on_finish()
